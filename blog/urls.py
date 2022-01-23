561b84bad50e4b8a7a2f5d0fc2f0835b72e319cc
Vladislav <vlad.narchyk@gmail.com>
2022-01-23 19:25:28 +0300

    4 commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 from blog.views import post_list, post_detail, social_links
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
                   path('', post_list, name='post_list'),
+                  re_path(r'^tag/(?P<tag_slug>[-\w]+)/$', post_list, name='post_list_by_tag'),
                   path('post/<int:pk>', post_detail, name='post_detail'),
-                  path('social_links', social_links, name='social_links')
+                  path('social_links', social_links, name='social_links'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, get_object_or_404
+
+from blog.forms import CommentForm
 from blog.models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from taggit.models import Tag
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     posts = Post.objects.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)  # спросить на занятии
+        posts = posts.filter(tags__in=[tag])
+
     paginator = Paginator(posts.order_by('-id'), 2)
     page = request.GET.get('page')
     page_obj = paginator.get_page(page)
@@ -14,12 +22,21 @@ def post_list(request):
         posts = paginator.page(1)
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
-    return render(request, 'post_list.html', {'page_obj': page_obj, 'posts': posts})
+    return render(request, 'post_list.html', {'page_obj': page_obj, 'posts': posts, 'tag': tag})
 
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'post_detail.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'post_detail.html', {'post': post, 'comments': comments, 'comment_form': comment_form})
 
 
 def social_links(request):
